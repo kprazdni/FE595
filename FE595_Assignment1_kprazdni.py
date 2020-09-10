cf87feaca91b12056abbcba07b08192b0ac01e8a
kprazdni <36051311+kprazdni@users.noreply.github.com>
2020-09-09 21:27:20 -0400

    Update FE595_Assignment1_kprazdni.py

--- FE595_Assignment1_kprazdni.py
+++ FE595_Assignment1_kprazdni.py
@@ -1,16 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#create period
 x = np.linspace(0,2*np.pi,1000,endpoint=True)
 cosine, sine = np.cos(x), np.sin(x)
 
+#plot sine and cosine
 plt.plot(x, sine, color = 'darkblue', label = 'Sine')
 plt.plot(x, cosine, color = 'darkred', label = 'Cosine')
 
+#indicate dimensions and tick names, customize plot
 plt.xlim(0, 2 * np.pi)
 plt.ylim(-2,2)
 plt.xticks([0, np.pi/2, np.pi, 3 * np.pi/2, 2*np.pi],[r'$0$', r'$+\pi/2$', r'$+\pi$', r'$+3\pi/2$', r'$+2\pi$'])
 plt.yticks([-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2])
+plt.title("Sine and Cosine Waves")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.grid(True, which = 'both')
 
 ax=plt.gca()
 ax.spines['right'].set_color('none')
