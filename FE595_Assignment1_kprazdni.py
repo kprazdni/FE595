2ff40dd68c0494ff58ee71d136accb03180a2869
kprazdni <36051311+kprazdni@users.noreply.github.com>
2020-09-13 22:01:14 -0400

    Update FE595_Assignment1_kprazdni.py

--- FE595_Assignment1_kprazdni.py
+++ FE595_Assignment1_kprazdni.py
@@ -1,32 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#create period
-x = np.linspace(0,2*np.pi,1000,endpoint=True)
-cosine, sine, tangent = np.cos(x), np.sin(x), np.tan(x)
+if __name__ == '__main__':
+    # create period
+    x = np.linspace(0, 2 * np.pi, 1000, endpoint=True)
+    cosine, sine, tangent = np.cos(x), np.sin(x), np.tan(x)
 
-#plot sine, cosine and tangent
-plt.plot(x, sine, color = 'darkblue', label = 'Sine')
-plt.plot(x, cosine, color = 'darkred', label = 'Cosine')
-plt.plot(x, tangent, color = 'darkgreen', label = 'Tangent')
+    # plot sine, cosine and tangent
+    plt.plot(x, sine, color='lightblue', label='Sine')
+    plt.plot(x, cosine, color='pink', label='Cosine')
+    plt.plot(x, tangent, color='lightgreen', label='Tangent')
 
+    # indicate dimensions and tick names, customize plot
+    plt.xlim(0, 2 * np.pi)
+    plt.ylim(-2, 2)
+    plt.xticks([0, np.pi / 2, np.pi, 3 * np.pi / 2, 2 * np.pi],
+               [r'$0$', r'$+\pi/2$', r'$+\pi$', r'$+3\pi/2$', r'$+2\pi$'])
+    plt.yticks([-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2])
+    plt.title("Sine, Cosine and Tangent Curves")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.grid(True, which='both')
 
-#indicate dimensions and tick names, customize plot
-plt.xlim(0, 2 * np.pi)
-plt.ylim(-2,2)
-plt.xticks([0, np.pi/2, np.pi, 3 * np.pi/2, 2*np.pi],[r'$0$', r'$+\pi/2$', r'$+\pi$', r'$+3\pi/2$', r'$+2\pi$'])
-plt.yticks([-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2])
-plt.title("Sine, Cosine and Tangent Curves")
-plt.xlabel("Time")
-plt.ylabel("Amplitude")
-plt.grid(True, which = 'both')
-
-ax=plt.gca()
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.xaxis.set_ticks_position('bottom')
-ax.spines['bottom'].set_position(('data',0))
-ax.yaxis.set_ticks_position('left')#17
-ax.spines['left'].set_position(('data',0))
-plt.legend(loc='lower left')
-plt.show()
+    ax = plt.gca()
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.yaxis.set_ticks_position('left')  # 17
+    ax.spines['left'].set_position(('data', 0))
+    plt.legend(loc='lower left')
+    plt.show()
